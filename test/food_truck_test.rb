6bbb348fb7c9fd13d7c82186102de1dbe0d1724c
Priya Power <priyajag72@gmail.com>
2020-08-05 09:19:30 -0600

    Add test_it_can_check_stock

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -21,4 +21,8 @@ class FoodTruckTest < Minitest::Test
     assert_equal ({}), @food_truck.inventory
   end
 
+  def test_it_can_check_stock
+    assert_equal 0, @food_truck.check_stock(@item1)
+  end
+
 end
