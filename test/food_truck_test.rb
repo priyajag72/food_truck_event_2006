0638be5e7dd0a55e34d4dbfd2fc5d762b89b71ff
Priya Power <priyajag72@gmail.com>
2020-08-05 09:22:53 -0600

    Add test it can stock items with behavior, expected hash, and assertion checking inventory

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -25,4 +25,10 @@ class FoodTruckTest < Minitest::Test
     assert_equal 0, @food_truck.check_stock(@item1)
   end
 
+  def test_it_can_stock_items
+    @food_truck.stock(@item1, 30)
+    expected = {@item1 => 30}
+    assert_equal expected, @food_truck.inventory
+  end
+
 end
