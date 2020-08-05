7cb16b544fa54ff475793210eca4a9b70cb41d58
Priya Power <priyajag72@gmail.com>
2020-08-05 09:31:14 -0600

    Add minitest to EventTest with 4 items, 3 foodtrucks, 1 event object and test it exists

---
+++ test/event_test.rb
@@ -0,0 +1,29 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/item"
+require "./lib/food_truck"
+require "./lib/event"
+
+class EventTest < Minitest::Test
+
+  def setup
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+
+    @food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    @food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    @food_truck3 = FoodTruck.new("Palisade Peach Shack")
+
+    @event = Event.new("South Pearl Street Farmers Market")
+  end
+
+  def test_it_exists
+    assert_instance_of Event, @event
+  end
+
+  #def test_it_has_attributes
+  #end
+
+end
